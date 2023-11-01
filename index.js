7aecbadd2a1c1a64fd3857167345eb23832545ea
Dustin Clark <dclark2434@gmail.com>
2023-11-01 08:33:01 -0500

    update js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     async function updateCounter() {
         let response = await fetch("https://rbobt7qnffimjzkx43bgveeeza0vficp.lambda-url.us-east-1.on.aws/");
         let data = await response.json();
-        counter.innerHTML = ` Views: ${data}`;
+        counter.innerHTML = ` ${data}`;
     }
 
     // Call the updateCounter function when the DOM content is loaded
