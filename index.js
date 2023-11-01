5a34e5c0ad59ed789dc0f8524dede13a0585095a
Dustin Clark <dclark2434@gmail.com>
2023-11-01 08:29:06 -0500

    updated js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const counter = document.querySelector("#visitorCount")
-async function updateCounter() {
-    let response = await fetch("https://rbobt7qnffimjzkx43bgveeeza0vficp.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const counter = document.querySelector("#visitorCount");
+
+    async function updateCounter() {
+        let response = await fetch("https://rbobt7qnffimjzkx43bgveeeza0vficp.lambda-url.us-east-1.on.aws/");
+        let data = await response.json();
+        counter.innerHTML = ` Views: ${data}`;
+    }
+
+    // Call the updateCounter function when the DOM content is loaded
+    updateCounter();
+});
