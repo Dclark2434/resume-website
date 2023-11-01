877cbed920d1a6caff54d74fb9b10c62fd63dbc7
Dustin Clark <dclark2434@gmail.com>
2023-11-01 08:09:52 -0500

    fixed js bug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const counter = document.querySelector(".visitorCount")
+const counter = document.querySelector("#visitorCount")
 async function updateCounter() {
     let response = await fetch("https://rbobt7qnffimjzkx43bgveeeza0vficp.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
